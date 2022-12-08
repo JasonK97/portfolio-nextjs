295b95888532173beb922464685141c8267de256
Jason Kent <jasonbkent@gmail.com>
2022-12-08 10:19:51 -0700

    Set up NextUI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import '../styles/globals.css'
+import { createTheme, NextUIProvider } from '@nextui-org/react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+// 2. Call `createTheme` and pass your custom values
+const lightTheme = createTheme({
+  type: 'light',
+  // theme: {
+  //   colors: {...}, // optional
+  // }
+})
+
+const darkTheme = createTheme({
+  type: 'dark',
+  // theme: {
+  //   colors: {...}, // optional
+  // }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextThemesProvider
+    defaultTheme="system"
+    attribute="class"
+    value={{
+      light: lightTheme.className,
+      dark: darkTheme.className
+    }}
+  >
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  </NextThemesProvider>
+  )
 }
 
 export default MyApp
