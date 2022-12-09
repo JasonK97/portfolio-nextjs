7e155bfe335e4a64c8d8c5123758f55e7ce49439
Jason Kent <jasonbkent@gmail.com>
2022-12-09 11:44:57 -0700

    Built out specialties page

--- pages/specialties.jsx
+++ pages/specialties.jsx
@@ -1,22 +1,50 @@
 import React from 'react'
-import { MdConstruction } from 'react-icons/md'
+import { FaReact } from 'react-icons/fa'
+import { SiNextdotjs, SiJavascript, SiCplusplus, SiPostgresql, SiPython, SiCsswizardry, SiAndroidstudio } from 'react-icons/si'
+import { IoLogoVue } from 'react-icons/io5'
+import { TiHtml5 } from 'react-icons/ti'
 import { SEO } from '../components'
-import { Description, Main } from '../styles/styles'
+import { 
+  Main,
+  Heading,
+  WorkHeading,
+  PictureInfoGrid,
+  Container,
+} from '../styles/styles'
 
 export default function Specialties() {
+  const styles = { padding: '20px', fontSize: '5em' }
+
   return (
-    <div>
+    <Container>
       <SEO 
-        page='Specielties'
+        page='Specialties'
         description='Languages and technologies I have experience with.'
       />
 
       <Main>
-        <MdConstruction fontSize='6rem'/>
-        <Description>
-          I'm sorry, this page is still under construction. Check in later.
-        </Description>
+        <Heading>Specialties</Heading>
+
+        <WorkHeading>Current Tools</WorkHeading>
+
+        <PictureInfoGrid>
+          <FaReact style={styles} />
+          <SiNextdotjs style={styles} />
+        </PictureInfoGrid>
+
+        <WorkHeading>Tools used in University</WorkHeading>
+
+        <PictureInfoGrid>
+          <SiCplusplus style={styles} />
+          <IoLogoVue style={styles} />
+          <SiJavascript style={styles} />
+          <TiHtml5 style={styles} />
+          <SiCsswizardry style={styles} />
+          <SiPostgresql style={styles} />
+          <SiAndroidstudio style={styles} />
+          <SiPython style={styles} />
+        </PictureInfoGrid>
       </Main>
-    </div>
+    </Container>
   )
 }
