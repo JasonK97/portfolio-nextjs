1fb78f5b5ab75d1daa13f85a71bd65b2e3c638e0
Jason Kent <jasonbkent@gmail.com>
2022-12-09 10:28:25 -0700

    Migrating styles to styled-components

--- styles/styles.js
+++ styles/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
-import { Link } from '@nextui-org/react'
+import { Link as NextLink } from '@nextui-org/react'
+import Link from 'next/link'
 
 export const Container = styled.div`
   padding: 0 2rem;
@@ -17,6 +18,18 @@ export const Description = styled.h2`
   max-width: 700px;
 `;
 
+export const WorkHeading = styled.h2`
+  margin: 2rem auto .5rem auto;
+  line-height: 1.5;
+  font-size: 1.5rem;
+  max-width: 700px;
+`;
+
+export const DateRange = styled.h4`
+  text-align: center;
+  font-size: 1rem;
+`;
+
 export const Main = styled.main`
   min-height: 100vh;
   padding: 4rem 0;
@@ -27,6 +40,15 @@ export const Main = styled.main`
   align-items: center;
 `;
 
+export const PictureInfoGrid = styled.div`
+  max-width:800px;
+  margin: 1rem 4rem;
+  line-height: 1.5;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+`;
+
 export const TextBodyContainer = styled.div`
   margin: 1rem 4rem;
   line-height: 1.5;
@@ -34,8 +56,25 @@ export const TextBodyContainer = styled.div`
   max-width: 700px;
 `;
 
-export const NavbarLink = styled(Link)`
+export const NavbarLink = styled(NextLink)`
   color: #FFFFFF
+`;
+
+export const StyledLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const ListContainer = styled.ul`
+  max-width: 900px;
+`;
+
+export const ListItem = styled.li`
+  line-height: 1.5;
+  font-size: 1rem;
+  display: list-item;
+  list-style-type: circle;
 `;
 
 export const Footer = styled.footer`
