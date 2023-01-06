448e3f6590b228272f44f55066fe376466ecba15
Jason Kent <jasonbkent@gmail.com>
2023-01-06 16:54:29 -0700

    building out page

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { SEO } from '../../components'
 
-export const BlogPage = () => {
+import { 
+  Container,
+  Heading, 
+  Main,
+  Description,
+  TextBodyContainer,
+} from '../../styles/styles'
+
+export default function BlogPage() {
   return (
-    <>
+    <Container>
       <SEO 
         page='Blog'
         description='Follow me on my development journey as I write articles about things I learn.'
       />
-    </>
+
+      <Main>
+        <Heading>Blog</Heading>
+      </Main>
+    </Container>
   )
 }
