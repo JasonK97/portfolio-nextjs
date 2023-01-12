b366f832572423a98a04bcada8d59d60c65115f9
Jason Kent <jasonbkent@gmail.com>
2023-01-12 12:47:51 -0700

    reordered alphabetically

--- styles/styles.js
+++ styles/styles.js
@@ -2,13 +2,50 @@ import styled from 'styled-components'
 // import { Link as NextLink } from '@nextui-org/react'
 import Link from 'next/link'
 
+export const BlogBodyContainer = styled.div`
+  margin: 1rem 4rem;
+  line-height: 1.5;
+  font-size: 1.5rem;
+  max-width: 800px;
+  @media(max-width: 800px) {
+    margin: .25rem auto .25rem auto;
+    display: grid;
+    justify-content: center;
+    width: 300px;
+  }
+`;
+
+export const BlogImage = styled.img`
+  padding: 1rem 0;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 60%;
+  @media(max-width: 800px) {
+    margin: 0;
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+  }
+`
+
+export const CodeText = styled.code`
+  background: #fafafa;
+  border-radius: 5px;
+  padding: 0.75rem;
+  font-size: 1.1rem;
+  font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
+    Bitstream Vera Sans Mono, Courier New, monospace;
+`;
+
 export const Container = styled.div`
   padding: 0 2rem;
 `;
 
-export const Heading = styled.h1`
-  line-height: 1.15;
-  font-size: 4rem;
+export const DateRange = styled.h4`
+  text-align: center;
+  font-size: 1rem;
 `;
 
 export const Description = styled.h2`
@@ -18,21 +55,33 @@ export const Description = styled.h2`
   max-width: 700px;
 `;
 
-export const WorkHeading = styled.h2`
-  margin: 2rem auto .5rem auto;
-  line-height: 1.5;
-  font-size: 1.5rem;
-  max-width: 700px;
+export const Footer = styled.footer`
+  display: flex;
+  flex: 1;
+  padding: 2rem;
+  border-top: 1px solid #eaeaea;
+  align-items: center;
 `;
 
-export const DateRange = styled.h4`
-  text-align: center;
+export const Heading = styled.h1`
+  line-height: 1.15;
+  font-size: 4rem;
+`;
+
+export const ListContainer = styled.ul`
+  max-width: 900px;
+`;
+
+export const ListItem = styled.li`
+  line-height: 1.5;
   font-size: 1rem;
+  display: list-item;
+  list-style-type: circle;
 `;
 
 export const Main = styled.main`
   min-height: 100vh;
-  padding: 4rem 0;
+  padding: 0 0 4rem 0;
   flex: 1;
   display: flex;
   flex-direction: column;
@@ -55,6 +104,12 @@ export const PictureInfoGrid = styled.div`
   }
 `;
 
+export const StyledLink = styled(Link)`
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
 export const TextBodyContainer = styled.div`
   margin: 1rem 4rem;
   line-height: 1.5;
@@ -67,36 +122,9 @@ export const TextBodyContainer = styled.div`
   }
 `;
 
-export const StyledLink = styled(Link)`
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-export const ListContainer = styled.ul`
-  max-width: 900px;
-`;
-
-export const ListItem = styled.li`
+export const WorkHeading = styled.h2`
+  margin: 2rem auto .5rem auto;
   line-height: 1.5;
-  font-size: 1rem;
-  display: list-item;
-  list-style-type: circle;
-`;
-
-export const CodeText = styled.code`
-  background: #fafafa;
-  border-radius: 5px;
-  padding: 0.75rem;
-  font-size: 1.1rem;
-  font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
-    Bitstream Vera Sans Mono, Courier New, monospace;
-`;
-
-export const Footer = styled.footer`
-  display: flex;
-  flex: 1;
-  padding: 2rem;
-  border-top: 1px solid #eaeaea;
-  align-items: center;
+  font-size: 1.5rem;
+  max-width: 700px;
 `;
