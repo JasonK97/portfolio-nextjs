d131f0e9434dff4075431ac35f7046c8a52ce7ae
Jason Kent <jasonbkent@gmail.com>
2023-01-25 10:37:39 -0700

    optimized header

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,35 +1,9 @@
 import React from 'react'
 import { Navbar as NextNavbar, Text, Link as NextLink } from '@nextui-org/react'
+import { collapseItems, socialLinks } from './data'
 import { Layout } from './styles'
 
 export const Navbar = () => {
-
-	const collapseItems = [
-		{
-			title: 'Home',
-			link: '/',
-		},
-		{
-			title: 'About',
-			link: '/about',
-		},
-		{
-			title: 'Projects',
-			link: '/projects',
-		},
-		{
-			title: 'Work Experience',
-			link: '/work',
-		},
-		{
-			title: 'Specialties',
-			link: '/specialties',
-		},
-		{
-			title: 'Blog',
-			link: '/blog',
-		},
-	]
 
 	return (
 		// <Layout>
@@ -43,43 +17,16 @@ export const Navbar = () => {
 					</a>
 				</NextNavbar.Brand>
 				<NextNavbar.Content hideIn="xs">
-					{/* <NextNavbar.Link href='/'>Home</NextNavbar.Link>
-					<NextNavbar.Link href='/about'>About</NextNavbar.Link>
-					<NextNavbar.Link href='/projects'>Projects</NextNavbar.Link>
-					<NextNavbar.Link href='/work'>Work</NextNavbar.Link>
-					<NextNavbar.Link href='/specialties'>Specialties</NextNavbar.Link> */}
-					<NextLink href='/'>Home</NextLink>
-					<NextLink href='/about'>About</NextLink>
-					<NextLink href='/projects'>Projects</NextLink>
-					<NextLink href='/work'>Work</NextLink>
-					<NextLink href='/specialties'>Specialties</NextLink>
-					<NextLink href='/blog'>Blog</NextLink>
+					{collapseItems.map(item => (
+							<NextLink key={item.title} href={item.link}>{item.title}</NextLink>
+					))}
 				</NextNavbar.Content>
 				<NextNavbar.Content>
-					<NextLink href='https://www.github.com/JasonK97' target='_blank'>
-						<img 
-							src='/static/github.png'
-							alt='GitHub Logo'
-							width='20px'
-							height='20px'
-						/>
-					</NextLink>
-					<NextLink href='https://www.linkedin.com/in/jasonbkent' target='_blank'>
-						<img 
-							src='/static/linkedin.png'
-							alt='LinkedIn Logo'
-							width='20px'
-							height='20px'
-						/>
-					</NextLink>
-					<NextLink href='https://www.twitter.com/JasonKent_' target='_blank'>
-						<img 
-							src='/static/twitter.png'
-							alt='Twitter Logo'
-							width='20px'
-							height='20px'
-						/>
-					</NextLink>
+					{socialLinks.map(({ link, image, alt }) => (
+						<NextLink key={alt} href={link} target='_blank'>
+							<img src={image} alt={alt} width='20px' height='20px' />
+						</NextLink>
+					))}
 				</NextNavbar.Content>
 				<NextNavbar.Collapse>
 					{collapseItems.map((item, index) => (
---
+++ components/Navbar/data/navBarData.js
@@ -0,0 +1,44 @@
+export const collapseItems = [
+  {
+    title: 'Home',
+    link: '/',
+  },
+  {
+    title: 'About',
+    link: '/about',
+  },
+  {
+    title: 'Projects',
+    link: '/projects',
+  },
+  {
+    title: 'Work Experience',
+    link: '/work',
+  },
+  {
+    title: 'Specialties',
+    link: '/specialties',
+  },
+  {
+    title: 'Blog',
+    link: '/blog',
+  },
+]
+
+export const socialLinks = [
+  {
+    link: 'https://www.github.com/JasonK97',
+    image: '/static/github.png',
+    alt: 'GitHub Logo',
+  },
+  {
+    link: 'https://www.linkedin.com/in/jasonbkent',
+    image: '/static/linkedin.png',
+    alt: 'LinkedIn Logo',
+  },
+  {
+    link: 'https://www.twitter.com/JasonKent_',
+    image: '/static/twitter.png',
+    alt: 'Twitter Logo',
+  },
+]
