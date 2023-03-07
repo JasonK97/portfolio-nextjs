98046c90f2b2fa71dfb891c8e0261ad8323ad366
Jason Kent <jasonbkent@gmail.com>
2023-03-07 11:58:49 -0700

    SEO tag optimization

--- components/SEO/SEO.jsx
+++ components/SEO/SEO.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Head from 'next/head'
+import NextHead from 'next/head'
 
 export const SEO = ({ page, description }) => {
   return (
-    <Head>
-      <title>Jason Kent | {page}</title>
+    <NextHead>
+      <title>{page ? page : 'Page'} | Jason Kent</title>
       <meta name="description" content={description ? description : 'Jason Kent\'s Portfolio Website'} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/static/favicon.ico" />
-    </Head>
+    </NextHead>
   )
 }
