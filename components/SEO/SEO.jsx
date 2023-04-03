b647ddca166c23f3d66622f33f91c101bc45d13a
Jason Kent <jasonbkent@gmail.com>
2023-04-03 10:26:27 -0600

    Added twitter meta tags

--- components/SEO/SEO.jsx
+++ components/SEO/SEO.jsx
@@ -7,8 +7,14 @@ export const SEO = ({ page, description, image, alt }) => {
       <title>{page ? page : 'Page'} | Jason Kent</title>
       <meta property="og:title" content={page ? page : 'Jason Kent Portfolio'} />
       <meta property="og:description" content={description ? description : 'Jason Kent\'s Portfolio Website'} />
-      <meta property="og:image" content={image ? image : '/static/me-blackbg.png'}/>
+      <meta property="og:image" content={image ? image : '/static/me-blackbg.png'} />
       <meta property="og:image:alt" content={alt ? alt : 'Jason Kent Logo'} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={page ? page : 'Jason Kent Portfolio'} />
+      <meta name="twitter:description" content={description ? description : 'Jason Kent\'s Portfolio Website'} />
+      <meta name="twitter:image" content={image ? image : '/static/me-blackbg.png'} />
+
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/static/favicon.ico" />
     </NextHead>
